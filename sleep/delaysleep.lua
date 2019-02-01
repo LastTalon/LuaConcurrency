5d7c2969e1f52f0449606c63c3258c1f3640cafe
Lucas Gangstad <last_talon@new.rr.com>
2019-02-01 16:25:25 -0600

    Remove unnecessary parameter passing

--- sleep/delaysleep.lua
+++ sleep/delaysleep.lua
@@ -1,10 +1,10 @@
 local function sleep(duration)
 	local co = coroutine.running()
 	local fulfilled = false
-	delay(duration, function (...)
+	delay(duration, function()
 		fulfilled = true
 		if coroutine.status(co) == "suspended" then
-			coroutine.resume(co, ...)
+			coroutine.resume(co)
 		end
 	end)
 	while not fulfilled do
